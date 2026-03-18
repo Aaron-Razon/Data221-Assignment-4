731120ca46d178143e41fab18ca570e9ee8a33a8
Aaron-Razon <aaron.anthony.r.razon@gmail.com>
2026-03-17 18:06:07 -0600

    Add neural network setup and training

--- Data221Assignment4Question5.py
+++ Data221Assignment4Question5.py
@@ -2,12 +2,16 @@
 
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 
 # === Set Random Seeds for Reproducibility ===
 np.random.seed(42)
-tf.random.set_random_seed(42)
+tf.random.set_seed(42)
 
 # === Load the Dataset ===
 breast_cancer_dataset = load_breast_cancer()
@@ -17,7 +21,7 @@ feature_matrix_X = breast_cancer_dataset.data
 target_vector_y = breast_cancer_dataset.target
 
 # === Split the Data into Training and Testing Sets ===
-training_feature_matrix_X, test_feature_matrix_X, training_target_vector_y, test_target_vector_y = train_test_split(
+training_feature_matrix_X, testing_feature_matrix_X, training_target_vector_y, testing_target_vector_y = train_test_split(
     feature_matrix_X,
     target_vector_y,
     test_size=0.2,
@@ -25,3 +29,40 @@ training_feature_matrix_X, test_feature_matrix_X, training_target_vector_y, test
     stratify=target_vector_y
 )
 
+# === Neural Network ===
+
+# === Standardize Input Features ===
+feature_scaler = StandardScaler()
+
+standardized_training_feature_matrix_X = feature_scaler.fit_transform(training_feature_matrix_X)
+standardized_testing_feature_matrix_X = feature_scaler.fit_transform(testing_feature_matrix_X)
+
+# === Build the Neural Network Model ===
+binary_classification_neural_network_model = tf.keras.Sequential([
+    tf.keras.Input(shape=(standardized_training_feature_matrix_X.shape[1],)),
+    tf.keras.layers.Dense(16, activation="relu"),
+    tf.keras.layers.Dense(1, activation="sigmoid"),
+])
+
+# === Compile the Model ===
+binary_classification_neural_network_model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+
+# === Train the Model ===
+binary_classification_neural_network_model.fit(
+    standardized_training_feature_matrix_X,
+    training_target_vector_y,
+    epochs=30,
+    batch_size=32,
+    verbose=0
+)
+
+
+# === Generate Neural Network Prediction Possibilities ===
+neural_network_prediction_possibilities = binary_classification_neural_network_model.predict(
+    standardized_testing_feature_matrix_X,
+    verbose=0
+)
