de63d763c6062750b1ec63fb9344db5ae42cc0a4
Aaron-Razon <aaron.anthony.r.razon@gmail.com>
2026-03-18 13:20:20 -0600

    Normalize image data and reshape inputs

--- Data221Assignment4Question6.py
+++ Data221Assignment4Question6.py
@@ -5,3 +5,10 @@ from tensorflow.keras.datasets import fashion_mnist
 # === Load the Fashion MNIST Dataset ===
 (training_images_X, training_labels_y), (testing_images_X, testing_labels_y) = fashion_mnist.load_data()
 
+# === Normalize the Pixel Values to the Range [0, 1] ===
+training_images_X = training_images_X / 255.0
+testing_images_X = testing_images_X / 255.0
+
+# === Reshape the Images to Include a Channel Dimension ===
+training_images_X = training_images_X.reshape(60000, 28, 28, 1)
+testing_images_X = testing_images_X.reshape(10000, 28, 28, 1)
