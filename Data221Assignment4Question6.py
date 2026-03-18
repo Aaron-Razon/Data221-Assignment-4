fd4372802ba6762986a1082cf214c618157f82e2
Aaron-Razon <aaron.anthony.r.razon@gmail.com>
2026-03-18 13:15:32 -0600

    Load the Fashion MNIST Dataset

---
+++ Data221Assignment4Question6.py
@@ -0,0 +1,7 @@
+# Q6 - Convolutional Neural Network with Fashion MNIST
+
+from tensorflow.keras.datasets import fashion_mnist
+
+# === Load the Fashion MNIST Dataset ===
+(training_images_X, training_labels_y), (testing_images_X, testing_labels_y) = fashion_mnist.load_data()
+
